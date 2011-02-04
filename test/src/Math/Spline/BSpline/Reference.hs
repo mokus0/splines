7daad588e3c49c12778dfbb2598952a22f4f251f
James Cook <mokus@deepbondi.net>
2011-02-04 20:04:22 +0000

    Refactored reference implementation of basis splines

--- test/src/Math/Spline/BSpline/Reference.hs
+++ test/src/Math/Spline/BSpline/Reference.hs
@@ -4,39 +4,79 @@
 module Math.Spline.BSpline.Reference where
 
 import Math.Spline.Knots
+import Math.Polynomial (Poly)
 import qualified Math.Polynomial as Poly
-import Data.VectorSpace
 
-ind True  = 1
-ind False = 0
+bases :: (Fractional a, Ord a) => Knots a -> a -> [[a]]
+bases kts x = coxDeBoor kts interp initial
+    where
+        ind True  = 1
+        ind False = 0
+
+        initial = 
+            [ ind (t_j <= x && x < t_jp1)
+            | (t_j, t_jp1) <- knotSpans kts 1
+            ]
+        interp t_j d0 b_nm1_j t_jpnp1 d1 b_nm1_jp1
+            = (if d0 == 0 then 0 else (x       - t_j) / d0) * b_nm1_j
+            + (if d1 == 0 then 0 else (t_jpnp1 -   x) / d1) * b_nm1_jp1
+
+basisFunctions :: (Fractional a, Ord a) => Knots a -> [[a -> a]]
+basisFunctions kts = coxDeBoor kts interp initial
+    where
+        ind True  = 1
+        ind False = 0
+
+        initial = 
+            [ \x -> ind (t_j <= x && x < t_jp1)
+            | (t_j, t_jp1) <- knotSpans kts 1
+            ]
+        interp t_j d0 b_nm1_j t_jpnp1 d1 b_nm1_jp1 x
+            = (if d0 == 0 then 0 else (x       - t_j)     / d0) * b_nm1_j   x
+            + (if d1 == 0 then 0 else (t_jpnp1 -   x) / d1) * b_nm1_jp1 x
+
+basisPolynomialsAt :: (Fractional a, Ord a) => Knots a -> a -> [[Poly a]]
+basisPolynomialsAt kts x = coxDeBoor kts interp initial
+    where
+        ind True  = Poly.one
+        ind False = Poly.zero
+        
+        initial = 
+            [ ind (t_j <= x && x < t_jp1)
+            | (t_j, t_jp1) <- knotSpans kts 1
+            ]
+        interp t_j d0 b_nm1_j t_jpnp1 d1 b_nm1_jp1
+            = (if d0 == 0 then Poly.zero else (Poly.x                 - Poly.constPoly t_j) / d0) * b_nm1_j
+            + (if d1 == 0 then Poly.zero else (Poly.constPoly t_jpnp1 -             Poly.x) / d1) * b_nm1_jp1
+            where
+                infixl 6 +, -
+                p + q   = Poly.addPoly p q
+                p - q   = p + (Poly.negatePoly q)
+                
+                infixl 7 *, /
+                p * q   = Poly.multPoly p q
+                p / s   = Poly.scalePoly (recip s) p
+
+-- This is a straightforward implementation of the Cox-De Boor recursion scheme
+-- generalized in a slightly strange way; the initial vector is a parameter 
+-- and the actual computation of the recursion step is a function parameter.
+-- The purpose is to allow the same recursion to be applied when computing basis
+-- function values and  basis polynomials.
+coxDeBoor kts interp initial = table
+    where
+        ts = knots kts
+        table = initial :
+            [ [ interp t_j d0 b_nm1_j t_jpnp1 d1 b_nm1_jp1
+              | (b_nm1_j, b_nm1_jp1)    <- spans 1 prevBasis
+              | (d0, d1)                <- spans 1 (spanDiffs n ts)
+              | (t_j, t_jpnp1)          <- spans (n+1) ts
+              ]
+            | prevBasis <- takeWhile (not . null) table
+            | n <- [1..]
+            ]
 
 spans :: Int -> [a] -> [(a,a)]
 spans     = spansWith (,)
 spanDiffs :: Num a => Int -> [a] -> [a]
 spanDiffs = spansWith subtract
 spansWith f n ts = zipWith f ts (drop n ts)
-
--- Implementation following the usual definition, which is not how it's 
--- usually actually implemented - uppermost knot is not in domain of basis
--- functions.
--- TODO: cleanup from generalization, test 'basisPolynomialsAt'
-bases kts x = basesAt kts x (/) id x
-basisPolynomialsAt kts x = basesAt kts x (^/) Poly.constPoly Poly.x
-basesAt kts xLoc (/) lift xVal = bs
-    where
-        ts = knots kts
-        
-        b = [ ind (t_j <= xLoc && xLoc < t_jp1)
-            | (t_j, t_jp1)     <- spans 1 ts
-            ]
-        
-        bs = b : takeWhile (not . null)
-            [ [ (if d0 == 0 then 0 else (xVal         - lift t_j) / d0) * b_nm1_j
-              + (if d1 == 0 then 0 else (lift t_jpnp1 -     xVal) / d1) * b_nm1_jp1
-              | (b_nm1_j, b_nm1_jp1)    <- spans 1 prevBasis
-              | (d0, d1)                <- spans 1 (spanDiffs n ts)
-              | (t_j, t_jpnp1)          <- spans (n+1) ts
-              ]
-            | prevBasis <- bs
-            | n <- [1..]
-            ]
