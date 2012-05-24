6c6dcd5078631f69a9c52dfdf7b8de0acfe5b2d1
Alex Lang <me@alang.ca>
2012-05-24 16:18:32 +0900

    Benchmarks to test Boxed vs Unboxed vectors

--- benchmark/DeBoor.hs
+++ benchmark/DeBoor.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 import Criterion.Main
 
 import Data.List (find)
-import qualified Data.Vector as V
+import qualified Data.Vector.Generic.Safe as V
+import qualified Data.Vector.Safe as BV
+import qualified Data.Vector.Unboxed.Safe as UV
 import Math.Spline.BSpline
 import Math.Spline.BSpline.Reference
 import Math.Spline.Knots
@@ -15,8 +18,12 @@ import Control.Monad
 kts = mkKnots $ [0,0,0] ++ [0..10] ++ [11,11,11]
 ctPts = map sin [0..12]
 
-spline :: BSpline V.Vector Double
-spline = bSpline kts (V.fromList ctPts)
+unboxedSpline :: BSpline UV.Vector Double
+unboxedSpline = bSpline kts (V.fromList ctPts)
+
+boxedSpline :: BSpline BV.Vector Double
+boxedSpline = bSpline kts (V.fromList ctPts)
+
 
 intervalPoly :: [(Double, Double, Poly Double)]
 intervalPoly = map f $ zip3 dkts (tail dkts) (basisPolynomials kts)
@@ -24,11 +31,11 @@ intervalPoly = map f $ zip3 dkts (tail dkts) (basisPolynomials kts)
     dkts = distinctKnots kts
     f (begin, end, k) = (begin, end, ) . sumPolys $ zipWith scalePoly ctPts (k !! 3)
 
-applyDeBoor :: Double -> Double
-applyDeBoor = evalBSpline spline
+applyDeBoor :: V.Vector v Double => BSpline v Double -> Double -> Double
+applyDeBoor s = evalBSpline s
 
-applyNaturalDeBoor :: Double -> Double
-applyNaturalDeBoor = evalNaturalBSpline spline
+applyNaturalDeBoor :: V.Vector v Double =>  BSpline v Double -> Double -> Double
+applyNaturalDeBoor s = evalNaturalBSpline s
 
 applyPoly :: Double -> Double
 applyPoly x = maybe 0 (\(_,_,p) -> evalPoly p x) $ find (\(b,e,_) -> x >= b && x < e) intervalPoly
@@ -37,10 +44,18 @@ applyAndSum :: (Double -> Double) -> [Double] -> Double
 applyAndSum f = sum . map f
 
 main = defaultMain
-       [ bench "deBoor 1000" $ whnf (applyAndSum applyDeBoor) [0,0.01..10]
-       , bench "deBoor 10000" $ whnf (applyAndSum applyDeBoor) [0,0.001..10]
-       , bench "natural 1000" $ whnf (applyAndSum applyNaturalDeBoor) [0,0.01..10]
-       , bench "natural 10000" $ whnf (applyAndSum applyNaturalDeBoor) [0,0.001..10]
+       [ bgroup "Boxed"
+         [ bench "deBoor 1000" $ whnf (applyAndSum (applyDeBoor boxedSpline)) [0,0.01..10]
+         , bench "deBoor 10000" $ whnf (applyAndSum (applyDeBoor boxedSpline)) [0,0.001..10]
+         , bench "natural 1000" $ whnf (applyAndSum (applyNaturalDeBoor boxedSpline)) [0,0.01..10]
+         , bench "natural 10000" $ whnf (applyAndSum (applyNaturalDeBoor boxedSpline)) [0,0.001..10]
+         ]
+       , bgroup "Unboxed"
+         [ bench "deBoor 1000" $ whnf (applyAndSum (applyDeBoor unboxedSpline)) [0,0.01..10]
+         , bench "deBoor 10000" $ whnf (applyAndSum (applyDeBoor unboxedSpline)) [0,0.001..10]
+         , bench "natural 1000" $ whnf (applyAndSum (applyNaturalDeBoor unboxedSpline)) [0,0.01..10]
+         , bench "natural 10000" $ whnf (applyAndSum (applyNaturalDeBoor unboxedSpline)) [0,0.001..10]
+         ]
        , bench "poly 1000" $ whnf (applyAndSum applyPoly) [0,0.01..10]
        , bench "poly 10000" $ whnf (applyAndSum applyPoly) [0,0.001..10]
        ]
