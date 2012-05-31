3fcabad5a138c93df7658c176960e3b070b15225
James Cook <mokus@deepbondi.net>
2012-05-31 07:04:57 -0400

    Made scratch visualization slightly more interesting

--- utils/Scratch.hs
+++ utils/Scratch.hs
@@ -2,22 +2,26 @@ module Main where
 
 import Math.Spline
 import Math.Spline.BSpline
+import Math.Spline.BSpline.Reference
 import Gloss
 import Graphics.Gloss
 
 import qualified Data.Vector.Unboxed as UV
 
-kts = mkKnots $ [0,0,0,0,0,0,2,2.5,5,6,7,10,10,10,10,10]
+kts = mkKnots $ [0,0,0,0,0,0,0,2,2.5,5,6,7,10,10,10,10,10,10,10]
 ctPts = 
     [ (2, 1)
+    , (2, 2)
     , (1, 2)
     , (-0.5, -0.5)
-    , (-3, 1)
+    , (-3.4, 1.2)
     , (-1.5, -0.3)
     , (-3, -1)
     , (-0.8, -2.4)
+    , (-0.3, -2.5)
     , (1.2, -2.7)
     , (2, -0.5)
+    , (2, 1)
     ]
 
 f :: BSpline UV.Vector Point
@@ -35,11 +39,16 @@ main = do
         ]
     quickAnim 1200 800 $ \x' ->
         let x = x' `fracMod` 13
-         in   scale (1/12) (1/12) 
+         in   scale (1/8) (1/8) 
+            . translate 0 0.33
             $ pictures
                 [ axes (-10, -10) (10, 10)
                 , color (greyN 0.2) $ spline f
+                
+                , color (dark red) $ plot (evalNaturalBSpline f) 0.1 0 x
                 , color yellow $ plot (evalSpline f) 0.1 0 x
+                -- , color (dark green) $ plot (evalReferenceBSpline f) 0.1 0 x
+                
                 , pictures $ zipWith color deBoorColors
                     [ bumpyLine 0.1 (UV.toList l)
                     | l <- deBoor f x
