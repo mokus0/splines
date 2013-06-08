89057a4e48b961599df753efbf54765b464977a2
Daniel Bergey <bergey@alum.mit.edu>
2013-06-08 16:15:57 -0400

    Add tests for surfaceGrid
    add cylinder to Tests -- should not depend on Diagrams!

--- src/Math/Tests.hs
+++ src/Math/Tests.hs
@@ -1,14 +1,26 @@
 import Test.HUnit
 
+import Data.VectorSpace
 import Math.Spline.Knots
 import Math.Spline.BSpline
 import Math.Spline.NurbsSurface
 
-import Diagrams.ThreeD.Shapes
+cyl :: NurbsSurface
+cyl = NurbsSurface
+      kLinear
+      kCircle
+      [zipWith toH wts circ, zipWith toH wts $ map (^+^ zhat) circ] where
+        zhat = (0,0,1)
+        wts = concat. repeat $ [1, sqrt 2 / 2]
+        circ = [(1,0,0),   (1,1,0),
+                (0,1,0),   (-1,1,0),
+                (-1,0,0), (-1,-1,0),
+                (0,-1,0), (1,-1,0),
+                (1,0,0)]
 
 kLinear = mkKnots [0,0,1,1] -- degree 1
 
-kCircle = mkKnots [0,0,0,0.25,0.25,0.5,0.5,0.75,0.75,1,1,1]
+kCircle = mkKnots [0,0,0,0.25,0.25,0.5,0.5,0.75,0.75,1,1,1] -- degree 2
 
 knotsTest = test [
   "findSpan" ~: [
@@ -33,11 +45,34 @@ knotsTest = test [
                  "0.25" ~: expectedU2 0 ~=? basisFuns' 2 kCircle 0.25
                    ]]]
 
-nurbsSurface = "evalSurface" ~: [
-  "cylinder" ~: ["0,0" ~: (1,0,0) ~=? evalSurface cyl 0 0,
-                 "1,0" ~: (1,0,1) ~=? evalSurface cyl 1 0,
-                 "0,0.25" ~: (0,1,0) ~=? evalSurface cyl 0 0.25]]
+nurbsSurface = test [
+  "evalSurface" ~: [
+     "cylinder" ~: ["0,0" ~: Just (1,0,0) ~=? evalSurface cyl 0 0,
+                    "1,0" ~: Just (1,0,1) ~=? evalSurface cyl 1 0,
+                    "0,0.25" ~: Just (0,1,0) ~=? evalSurface cyl 0 0.25]],
+  "surfaceGrid" ~: [
+    -- only evaluates at knots
+    "cylinder" ~: ["2,5" ~: [[(1.0,0.0,0.0),(0.0,1.0,0.0),(-1.0,0.0,0.0),(0.0,-1.0,0.0),(1.0,0.0,0.0)],[(1.0,0.0,1.0),(0.0,1.0,1.0),(-1.0,0.0,1.0),(0.0,-1.0,1.0),(1.0,0.0,1.0)]] ~=? surfaceGrid cyl 2 5,
+                   -- circle is very non-uniform in v
+                   "2,9" ~: [
+                     [(1,0,0),(s22,s22,0),(0,1,0),(0-s22,s22,0),
+                      (-1,0,0),(0-s22,0-s22,0),(0,-1,0),(s22,0-s22,0),(1,0,0)],
+                     [(1,0,1),(s22,s22,1),(0,1,1),(0-s22,s22,1),
+                      (-1,0,1),(0-s22,0-s22,1),(0,-1,1),(s22,0-s22,1),(1,0,1)]]
+                     ~=? surfaceGrid cyl 2 9,
+                   -- z-axis is linear in u
+                   "3 9" ~: [
+                     [(1,0,0),(s22,s22,0),(0,1,0),(0-s22,s22,0),
+                      (-1,0,0),(0-s22,0-s22,0),(0,-1,0),(s22,0-s22,0),(1,0,0)],
+                     [(1,0,0.5),(s22,s22,0.5),(0,1,0.5),(0-s22,s22,0.5),
+                      (-1,0,0.5),(0-s22,0-s22,0.5),(0,-1,0.5),(s22,0-s22,0.5),(1,0,0.5)],
+                     [(1,0,1),(s22,s22,1),(0,1,1),(0-s22,s22,1),
+                      (-1,0,1),(0-s22,0-s22,1),(0,-1,1),(s22,0-s22,1),(1,0,1)]]
+                     ~=? surfaceGrid cyl 3 9]]]
 
 u2 n x = (1-x)**(2-n) * x**n
 
 expectedU2 x = [u2 0 x, 2 * (u2 1 x), u2 2 x]
+
+s22 :: Double
+s22 = sqrt 2 / 2
